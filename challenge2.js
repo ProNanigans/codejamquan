1cbf587ca7429cd71b19e400ad4edca18ed9ed80
ProNanigans <39745772+ProNanigans@users.noreply.github.com>
2019-06-06 20:38:08 -0400

    Update challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -1,5 +1,5 @@
 function nextEdge(firstS, secondS){
-    let thirdS = (firstS + secondS) -1
+    let thirdS = (firstS + secondS) -1//adds first side and second side then subtracts one because the largest triangle side can't be longer than the sum of the two other sides.
     return thirdS
 }
 console.log(nextEdge(8, 10))
